5354acd9c7a09d60859534db702d024ce40f53d3
Your Name <you@example.com>
2023-11-15 02:29:37 +0600

    finish chat

--- REACT/chat/src/App.js
+++ REACT/chat/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import {ChatEngine} from 'react-chat-engine'
-import './App.css'
-import ChatFeed from './components/ChatFeed'
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from './components/LoginForm'
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
-        height="100vh"
-        projectID="a3dc54ae-4656-4e55-b5d3-d12ebe84558e"
-        userName="Mishka"
-        userSecret="iloveArsen"
-        rendrChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      height="100vh"
+      projectID="a3dc54ae-4656-4e55-b5d3-d12ebe84558e"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      rendrChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- REACT/chat/src/components/ChatFeed.js
+++ REACT/chat/src/components/ChatFeed.js
@@ -8,6 +8,17 @@ const ChatFeed = (props) => {
 
   const chat = chats && chats[activeChat];
 
+  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
+    <div
+      key={`read_${index}`}
+      className="read-receipt"
+      style={{
+        float: isMyMessage ? 'right' : 'left',
+        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
+      }}
+    />
+  )); 
+
   const renderMessages = () => {
     const keys = Object.keys(messages);
 
@@ -20,12 +31,12 @@ const ChatFeed = (props) => {
             <div className="message-block">
                 {
                     isMyMessage 
-                    ? <MyMessage />
-                    : <TheirMessage />
+                    ? <MyMessage message={message}/>
+                    : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
                 }
             </div>
             <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
-
+                {renderReadReceipts(message, isMyMessage)}
             </div>
         </div>
     
